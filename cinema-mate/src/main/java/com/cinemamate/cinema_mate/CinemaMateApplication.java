0eab869b892caaf2c3ac883b5cdf046e1346fbde
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2024-12-31 04:50:42 +0300

    Cinema-20:initial dependency and data base configuration

---
+++ cinema-mate/src/main/java/com/cinemamate/cinema_mate/CinemaMateApplication.java
@@ -0,0 +1,13 @@
+package com.cinemamate.cinema_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaMateApplication.class, args);
+	}
+
+}
