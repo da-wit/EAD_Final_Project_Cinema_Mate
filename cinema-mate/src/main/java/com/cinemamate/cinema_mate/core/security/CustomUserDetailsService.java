a97a856971c996a0426fd93f023d27477688e23d
Dawit Belay <148384165+da-wit@users.noreply.github.com>
2025-01-04 00:30:38 +0300

    jwt auth filter implmented

--- cinema-mate/src/main/java/com/cinemamate/cinema_mate/core/security/CustomUserDetailsService.java
+++ cinema-mate/src/main/java/com/cinemamate/cinema_mate/core/security/CustomUserDetailsService.java
@@ -4,7 +4,9 @@ import com.cinemamate.cinema_mate.core.constant.Role;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
